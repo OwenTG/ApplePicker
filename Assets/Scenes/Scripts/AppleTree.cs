14b041c19dd0cba393d13c6a2399f67fb732b002
OwenTG <42875734+OwenTG@users.noreply.github.com>
2018-09-07 16:27:30 -0400

    Code x limiter added

--- Assets/Scenes/Scripts/AppleTree.cs
+++ Assets/Scenes/Scripts/AppleTree.cs
@@ -12,7 +12,7 @@ public class AppleTree : MonoBehaviour
     public float speed = 1f;
 
     // Distance where AppleTree turns around
-    public float leftAndRightEdge = 10f;
+    public float leftAndRightEdge = 20f;
 
     // Chance that the AppleTree will change directions
     public float chanceToChangeDirections = 0.1f;
@@ -28,6 +28,17 @@ public class AppleTree : MonoBehaviour
     private void Update()
     {
         // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
         // Changing Direction
+        if (pos.x < -leftAndRightEdge )
+        {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if ( pos.x > leftAndRightEdge )
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        }
     }
 }
