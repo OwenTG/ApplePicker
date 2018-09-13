90761348bdf6a0e338ec5a7e6fd64c6da0319055
OwenTG <G00249671@stu.raritanval.edu>
2018-09-13 17:21:40 -0400

    Moving the Baskets with the Mouse

---
+++ Assets/Scenes/Scripts/Basket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour {
+
+    private void Update()
+    {
+        //Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //The Camera's position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Move the x position of this basket of th x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+    }
